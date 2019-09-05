50d29cd29ed07780e4668df23bbdb631a7270069
mboulanger <mboulanger@codextime.com>
2019-09-05 11:09:28 +0200

    communication fonctionne avec les room separe

--- app.js
+++ app.js
@@ -20,13 +20,9 @@ app.get('/', (req, res) => {
 	res.render("index.ejs");
 });
 
-app.get('/:idRoom', (req, res) => {
-	res.render("buzz.ejs");
-});
-
-app.post('/app', (req,res)=>{
+app.post('/app', (req, res) => {
 	let admin = isAdmin(req.body.idRoom, req.body.playerId);
-	res.render('buzz.ejs', {admin: admin});
+	res.render('buzz.ejs', { admin: admin });
 });
 
 var salle = {};
@@ -40,6 +36,8 @@ wss.on("connection", function conncetion(ws) {
 	ws.on('message', function message(raw) {
 		let data = JSON.parse(raw);
 		let msg = {};
+		let idRoom = "";
+		let playerId;
 		switch (data.type) {
 			case 'create':
 				if (salleExist(data.id)) {
@@ -59,7 +57,7 @@ wss.on("connection", function conncetion(ws) {
 						temps: 0,
 						interval: undefined,
 					};
-					let playerId = 0;
+					playerId = 0;
 					playerId = createPlayer(data.id, 'admin', 'admin');
 					msg = {
 						type: 'create',
@@ -67,13 +65,12 @@ wss.on("connection", function conncetion(ws) {
 						result: 'ok',
 						playerId: playerId
 					}
-					ws.idRoom = data.id;
 					ws.send(JSON.stringify(msg));
 				}
 				break;
 			case 'join':
 				let result = 'nop';
-				let playerId = 0;
+				playerId = 0;
 				if (canJoin(data.id)) {
 					result = 'ok';
 					playerId = createPlayer(data.id, data.pseudo, 'player');
@@ -85,111 +82,110 @@ wss.on("connection", function conncetion(ws) {
 					result: result,
 					playerId: playerId
 				}
-				ws.idRoom = data.id;
 				ws.send(JSON.stringify(msg));
 				break;
+			case 'joined':
+				idRoom = data.idRoom;
+				playerId = data.playerId;
+				ws.idRoom = idRoom;
+				break;
 			case 'getPlayersAll':
-				{
-					let idRoom = data.idRoom;
-					let playerId = data.playerId;
-					if (salle[idRoom] != undefined && isAdmin(idRoom, playerId)) {
-						msg = {
-							type: 'getPlayersAll',
-							players: salle[idRoom].players
-						}
-						ws.send(JSON.stringify(msg));
+				idRoom = data.idRoom;
+				playerId = data.playerId;
+				if (salle[idRoom] != undefined && isAdmin(idRoom, playerId)) {
+					msg = {
+						type: 'getPlayersAll',
+						players: salle[idRoom].players
 					}
+					ws.send(JSON.stringify(msg));
 				}
 				break;
 			case 'start':
-				{
-					let idRoom = data.idRoom;
-					let playerId = data.playerId;
-					if (isAdmin(idRoom, playerId)) {
-						salle[idRoom].temps = data.temps;
-						salle[idRoom].etat = 'start';
-						if (data.temps != -1)
-							salle[idRoom].interval = setInterval((param) => {
-								if (param.temps > 0) {
-									param.temps--;
-								}
-								else {
-									param.etat = 'stop';
-									clearInterval(param.interval);
-								}
-							}, 1000, salle[idRoom]);
-						resetPlayersScore(idRoom);
-						msg = {
-							type: 'start',
-							temps: salle[idRoom].temps
-						}
-						wss.clients.forEach(function each(client) {
-							if (client.readyState === WebSocket.OPEN && client.idRoom == ws.idRoom) {
-								client.send(JSON.stringify(msg));
+				idRoom = data.idRoom;
+				playerId = data.playerId;
+				if (isAdmin(idRoom, playerId)) {
+					salle[idRoom].temps = data.temps;
+					salle[idRoom].etat = 'start';
+					clearInterval(salle[idRoom].interval);
+					if (data.temps != -1)
+						salle[idRoom].interval = setInterval((param) => {
+							if (param.temps > 0) {
+								param.temps--;
 							}
-						});
-					}
-				}
-				break;
-			case 'stop':
-				{
-					let idRoom = data.idRoom;
-					let playerId = data.playerId;
-					if (isAdmin(idRoom, playerId)) {
-						salle[idRoom].etat = 'stop';
-						clearInterval(salle[idRoom].interval);
-						msg = {
-							type: 'stop',
-							temps: salle[idRoom].temps
-						}
-						wss.clients.forEach(function each(client) {
-							if (client.readyState === WebSocket.OPEN && client.idRoom == ws.idRoom) {
-								client.send(JSON.stringify(msg));
+							else {
+								param.etat = 'stop';
+								clearInterval(param.interval);
 							}
-						});
-					}
-				}
-				break;
-			case 'reset':
-					{
-						let idRoom = data.idRoom;
-						let playerId = data.playerId;
-						if (isAdmin(idRoom, playerId)) {
-							salle[idRoom].temps = data.temps;
-							salle[idRoom].etat = 'stop';
-							clearInterval(salle[idRoom].interval);
-
-							msg = {
-								type: 'stop',
-								temps: salle[idRoom].temps
-							}
-							wss.clients.forEach(function each(client) {
-								if (client.readyState === WebSocket.OPEN && client.idRoom == ws.idRoom) {
-									client.send(JSON.stringify(msg));
-								}
-							});
-						}
-					}
-				break;
-			case 'buzz':
-				{
-					let idRoom = data.idRoom;
-					let playerId = data.playerId;
-
-					
-					let score = Date.now() - salle[idRoom].players[playerId].time;
-					score /= 1000;
-					
-					salle[idRoom].players[playerId].score = score;
+						}, 1000, salle[idRoom]);
+					resetPlayersScore(idRoom);
 					msg = {
-						type: 'buzz'
+						type: 'start',
+						temps: salle[idRoom].temps
 					}
+					ws.idRoom = idRoom;
 					wss.clients.forEach(function each(client) {
 						if (client.readyState === WebSocket.OPEN && client.idRoom == ws.idRoom) {
 							client.send(JSON.stringify(msg));
 						}
 					});
 				}
+				break;
+			case 'stop':
+				idRoom = data.idRoom;
+				playerId = data.playerId;
+				if (isAdmin(idRoom, playerId)) {
+					salle[idRoom].etat = 'stop';
+					clearInterval(salle[idRoom].interval);
+					msg = {
+						type: 'stop',
+						temps: salle[idRoom].temps
+					}
+					ws.idRoom = idRoom;
+					wss.clients.forEach(function each(client) {
+						if (client.readyState === WebSocket.OPEN && client.idRoom == ws.idRoom) {
+							client.send(JSON.stringify(msg));
+						}
+					});
+				}
+				break;
+			case 'reset':
+				idRoom = data.idRoom;
+				playerId = data.playerId;
+				if (isAdmin(idRoom, playerId)) {
+					salle[idRoom].temps = data.temps;
+					salle[idRoom].etat = 'stop';
+					clearInterval(salle[idRoom].interval);
+
+					msg = {
+						type: 'stop',
+						temps: salle[idRoom].temps
+					}
+					ws.idRoom = idRoom;
+					wss.clients.forEach(function each(client) {
+						if (client.readyState === WebSocket.OPEN && client.idRoom == ws.idRoom) {
+							client.send(JSON.stringify(msg));
+						}
+					});
+				}
+				break;
+			case 'buzz':
+				idRoom = data.idRoom;
+				playerId = data.playerId;
+
+
+				let score = Date.now() - salle[idRoom].players[playerId].time;
+				score /= 1000;
+
+				salle[idRoom].players[playerId].score = score;
+				msg = {
+					type: 'buzz'
+				}
+				ws.idRoom = idRoom;
+				wss.clients.forEach(function each(client) {
+					if (client.readyState === WebSocket.OPEN && client.idRoom == ws.idRoom) {
+						client.send(JSON.stringify(msg));
+					}
+				});
 				break;
 		}
 	});
@@ -227,14 +223,12 @@ function createPlayer(idRoom, pseudo, role) {
 	return id;
 }
 
-function resetPlayersScore(idRoom)
-{
+function resetPlayersScore(idRoom) {
 	let players = salle[idRoom].players;
-	for(let id in players)
-	{
+	for (let id in players) {
 		let player = players[id];
 		player.score = -1;
-		player.time = Date.now(); 
+		player.time = Date.now();
 	}
 }
 
@@ -246,7 +240,7 @@ function getListSalleId() {
 }
 
 function isAdmin(idRoom, playerId) {
-	return ( playerId == servId ||  salle[idRoom].players[playerId].role == 'admin');
+	return (playerId == servId || salle[idRoom].players[playerId].role == 'admin');
 }
 
 
